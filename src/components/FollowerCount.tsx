d5ad886a24d47d0410e7ff8abc71c9bfef66b1d9
Banibu <guicjs14@gmail.com>
2024-08-04 19:55:27 -0300

    Pequena correção

--- src/components/FollowerCount.tsx
+++ src/components/FollowerCount.tsx
@@ -17,7 +17,7 @@ export default function FollowerCount({
 
   return (
     <span>
-      Followers:{" "}
+      Seguidores:{" "}
       <span className="font-semibold">{formatNumber(data.followers)}</span>
     </span>
   );
